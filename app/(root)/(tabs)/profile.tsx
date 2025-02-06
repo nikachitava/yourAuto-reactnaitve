01d326743e8904f10ef6dcb086d7668c69b5ebd7
nikachitava <chitava18.official@gmail.com>
2025-02-06 23:01:05 +0400

    implement log out

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,10 +1,28 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { CustomButton } from "@/components/ui/CustomButton";
+import { removeToken } from "@/service/storage/authToken";
+import { router } from "expo-router";
 
 const ProfileScreen = () => {
+	const handleLogoOut = async () => {
+		try {
+			await removeToken();
+			router.push("/auth");
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	return (
 		<View>
 			<Text>ProfileScreen</Text>
+			<CustomButton
+				fullWidth
+				disabled={false}
+				label="Log out"
+				onPress={handleLogoOut}
+			/>
 		</View>
 	);
 };
