5fa721a5cc55ba89519a42be24e66ea566325327
nikachitava <chitava18.official@gmail.com>
2025-02-06 22:41:41 +0400

    change baseURL

--- hooks/useAxios.ts
+++ hooks/useAxios.ts
@@ -4,4 +4,7 @@ const baseUrl = process.env.EXPO_PUBLIC_BASE_URL;
 
 export const useAxios = axios.create({
     baseURL: baseUrl,
+    headers: {
+        'Content-Type': 'application/json'
+      }
 });
